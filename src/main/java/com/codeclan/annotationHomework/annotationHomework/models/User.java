ad9678e8f8169d54ad365d5b8b4e2a5a7dc48d25
Mark <nirvanafoo86@hotmail.com>
2019-05-21 17:14:54 +0100

    Creating properties on file

--- src/main/java/com/codeclan/annotationHomework/annotationHomework/models/User.java
+++ src/main/java/com/codeclan/annotationHomework/annotationHomework/models/User.java
@@ -1,4 +1,18 @@
 package com.codeclan.annotationHomework.annotationHomework.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
 }
